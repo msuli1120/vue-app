c56519bd2e7fb30d135f4d5333dc2b67804030e0
Xing Li <msuli_1120@ymail.com>
2017-06-13 09:06:47 -0700

    add css scoped

--- src/Vueapp.vue
+++ src/Vueapp.vue
@@ -1,7 +1,11 @@
 <template>
-  <ul>
-    <li v-for="name in names">{{ name }}</li>
-  </ul>
+  <div>
+    <h1>List of names</h1>
+    <ul>
+      <li v-for="name in names">{{ name }}</li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
@@ -14,6 +18,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  h1{
+    color: green;
+  }
 </style>
